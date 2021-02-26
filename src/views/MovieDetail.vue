9eeae89d9b65b82c6200d8347aec30ebbed59677
Gerard Sanga <sangagerard@pop-os.localdomain>
2021-02-27 00:25:52 +0300

    spinners for when movie screenings are being fetched from the backend

--- src/views/MovieDetail.vue
+++ src/views/MovieDetail.vue
@@ -3,39 +3,22 @@
     <BaseHeader/>
     <transition name='view' appear enter-active-class="animate__animated animate__fadeIn animate__dealy__slower" leave-active-class="animate__animated animate__fadeIn">
     <div class="container">
-
         <div class="row">
-
-          <!-- Post Content Column -->
           <div class="col-lg-8">
-
-            <!-- Title -->
             <h1 class="mt-4 text_color">{{ movie.title }}</h1>
-
-            <!-- Author -->
             <p class="lead text_color">
               Directed by
               <a href="#">{{ movie.director }}</a>
             </p>
-
             <hr>
-
-            <!-- Date/Time -->
             <p class="text_color">Premiered on {{ movie.premier_date }}</p>
-
             <hr>
-
-            <!-- Preview Image -->
             <img class="img-fluid rounded" v-bind:src="'http://localhost:8000' + movie.image" alt="" style="width:900px;height:300px;">
-
             <hr>
-
-            <!-- Post Content -->
             <p class="lead text_color">
               {{ movie.description}}
             </p>
             <hr>
-            <!-- Theaters Showing the movie -->
               <div v-if="screenings.length">
                 <div class="card my-4" v-for="(screening, index) in screenings" :key="index">
                   <h5 class="card-header">{{ screening.hall }} - {{ screening.theater }}</h5>
@@ -46,15 +29,16 @@
                   </div>
                 </div>
               </div>
-              <div v-else class="mx-auto text-center" style="color: #ff0f0f">
+              <div v-else-if="screenings.length == 0" class="mx-auto text-center" style="color: #ff0f0f">
                 <h2 class="mt-5">There are currently no screenings for this movie</h2>
               </div>
-              
-            <!-- End of theaters showing the movie -->
+              <div v-else>
+                <div class="spinner-border text-warning" role="status">
+                  <span class="sr-only">Loading...</span>
+                </div>
+              </div>
           </div>
         </div>
-        <!-- /.row -->
-
     </div>
   </transition>
   </div>
